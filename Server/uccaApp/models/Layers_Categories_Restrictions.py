48a5909da9686eb46bb52b4228b9b4d073eb8c9c
Yarin <yarin@zigit.co.il>
2017-03-30 10:15:25 +0300

    * upgrade max size of restriction in db

--- Server/uccaApp/models/Layers_Categories_Restrictions.py
+++ Server/uccaApp/models/Layers_Categories_Restrictions.py
@@ -6,8 +6,8 @@ class Layers_Categories_Restrictions(models.Model):
     id = models.AutoField(primary_key=True)
     layer_id = models.ForeignKey(Layers,related_name="resriction_layer_id",db_column="layer_id",on_delete=models.PROTECT)
     resriction_type = models.CharField(max_length=256,choices=Constants.RESTRICTION_TYPES)
-    category_ids1 = models.CharField(max_length=1000)
-    category_ids2 = models.CharField(max_length=1000)
+    category_ids1 = models.CharField(max_length=Constants.RESTRICTION_CATEGORIES_MAX_LENGTH)
+    category_ids2 = models.CharField(max_length=Constants.RESTRICTION_CATEGORIES_MAX_LENGTH)
 
 
     class Meta:
