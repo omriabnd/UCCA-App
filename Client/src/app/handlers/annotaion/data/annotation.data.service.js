7586598bb9dd572318ce6f3ac07bfc405adc9843
Talya Shraga <talyashra@gmail.com>
2018-07-02 13:57:42 +0300

    New, cleaner and working unit comparator.

--- Client/src/app/handlers/annotaion/data/annotation.data.service.js
+++ Client/src/app/handlers/annotaion/data/annotation.data.service.js
@@ -416,11 +416,11 @@
                 }
 
 
-                // Update indexInUnit attribute to tokens in units instead of unit 0. todo: Where the update to unit 0???
-                // debugger
-                newObject.tokens.forEach(function (token, index, inInit) {
-                    token.indexInUnit = index;
-                });
+                // // Update indexInUnit attribute to tokens in units instead of unit 0. todo: Where the update to unit 0???
+                // // debugger
+                // newObject.tokens.forEach(function (token, index, inInit) {
+                //     token.indexInUnit = index;
+                // });
 
                 /**
                  * This part computes index_int, which is the index where the unit will be inserted into the list
@@ -730,60 +730,23 @@
          * @param a - first unit
          * @param b - second unit
          * @returns {number} - according the priority of a
-         * BUGGY - sortAndUpdate calls to sortTree, sortTree calls to this function,
-         * sometimes parentUnit.tokens contain the tokens list, and sometimes parentUnit.tokenCopy contain the tokens list.
          */
-        function unitComparator(a, b){
-            trace("DataService - unitComparator");
-            var aParentUnit = getParentUnit(a.tree_id);
-            var bParentUnit = getParentUnit(b.tree_id);
-
-            var aElementPos = aParentUnit.tokens.map(function(x) {return x.static.id; }).indexOf(a.tokens[0].static.id);
-            var bElementPos = bParentUnit.tokens.map(function(x) {return x.static.id; }).indexOf(b.tokens[0].static.id);
-
-            if(a.unitType !== "REGULAR" || b.unitType !== "REGULAR"){ // At least, one is no regular
-                if(a.unitType === "REGULAR" && b.unitType === "IMPLICIT"){ // Implicit should be before regular
-                    return 1;
-                }else if((a.unitType === "REMOTE" && b.unitType === "REGULAR") || (b.unitType === "REMOTE"  && a.unitType === "REGULAR")){ // According to first start_index
-                    if (a.tokens[0].static.start_index > b.tokens[0].static.start_index) {
-                        return -1
-                    } else if (a.tokens[0].static.start_index < b.tokens[0].static.start_index) {
-                        return 1;
-                    }
-                    return 0;
-                }else if(b.unitType === "REGULAR" && a.unitType !== "REGULAR"){
-                    return -1;
-                }else{  // Both not regular
-                    if(a.unitType === "REMOTE" && b.unitType === "IMPLICIT"){ // Implicit should be before remote
-                        return 1;
-                    }else if(a.unitType === "IMPLICIT" && b.unitType === "REMOTE"){
-                        return -1; // implicit should be first
-                    }else{ // Both remote or both implicit
-                        if(a.unitType === "REMOTE" && b.unitType === "REMOTE"){ // Both remote
-                            console.log("A and B are remotes", a.cloned_from_tree_id, b.cloned_from_tree_id,  "  remotes ids:", a.remote_original_id, b.remote_original_id)
-                            if(a.cloned_from_tree_id > b.remote_original_id){ // TODO: when adding a new remote unit, there is no cloned_from_tree_id attribute, instead of this there is remote_original_id.
-                                return 1;
-                            }
-                            else if(a.cloned_from_tree_id < b.remote_original_id){
-                                return -1;
-                            }else{
-                                return 0;  // TODO: Add to assertion service - fail if two remote units have the same parent and clone_from_id
-                            }
-                        }
-                        return 0; // TODO: compare based on an ID, so the sort is stable  - both IMPLICIT
-                    }
-                }
+        function unitComparator(a, b) {
+            // Both implicit, the order doesn't matter much but we need to use something to make the sort stable
+            if(a.unitType === 'IMPLICIT' && b.unitType === 'IMPLICIT') {
+                return (a.tree_id < b.tree_id ? -1 : 1);  // a and b are never the same unit
             }
-            // tokenCopy is not contain indexInUnit attribute, with remote units- tokens=[]---error: Cannot read property 'indexInUnit' of undefined
-            else {  // Both are regular units
-                if(aParentUnit.tokens[aElementPos].indexInUnit > bParentUnit.tokens[bElementPos].indexInUnit){
-                    return 1;
-                }else if(aParentUnit.tokens[aElementPos].indexInUnit < bParentUnit.tokens[bElementPos].indexInUnit){
-                    return -1;
-                }else{
-                    return 0;
-                }
+
+            // One is implicit, the other isn't - the implicit is first
+            if(a.unitType === 'IMPLICIT') {
+                return -1;
             }
+            if(b.unitType === 'IMPLICIT') {  // B is implicit, A isin't.
+                return 1;
+            }
+
+            // None implicit, order based on the order of tokens in the task
+            return a.tokens[0].static.index_in_task < b.tokens[0].static.index_in_task ? -1 : 1;
         }
 
         function tokenStartIndexInParent(token){
@@ -1243,6 +1206,7 @@
          * @returns {*} - parent unit id
          */
         function getParentUnitId(unitId) {
+            // TODO: Remove this function altogether, getting the parent tree id is now as simple as looking at a property
             trace("DataService - getParentUnitId");
             if(unitId === null){
                 return null;
