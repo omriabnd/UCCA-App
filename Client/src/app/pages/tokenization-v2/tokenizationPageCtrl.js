a655377992ff8fe2a5106385af16443fe9781fef
talyashra <talyashra@gmail.com>
2019-09-24 13:22:04 +0300

    Fixing the bug in tokenization in Hebrew. (comment unnecessary functions)

--- Client/src/app/pages/tokenization-v2/tokenizationPageCtrl.js
+++ Client/src/app/pages/tokenization-v2/tokenizationPageCtrl.js
@@ -36,8 +36,8 @@
       uccaFactory.originalText = $scope.originalText;
 
 
-      uccaFactory.setOriginalTextNoSpaces();
-      uccaFactory.setOriginalTextSpaceMap();
+      // uccaFactory.setOriginalTextNoSpaces();
+      // uccaFactory.setOriginalTextSpaceMap();
 
       console.log(uccaFactory.originalText, uccaFactory.originalTextNoSpaces, uccaFactory.originalTextSpaceMap);
 
@@ -74,7 +74,7 @@
        *
        * */
 
-      $scope.originalTokens = uccaFactory.getTokensFromText($scope.originalText);
+      // $scope.originalTokens = uccaFactory.getTokensFromText($scope.originalText);
 
       $scope.tokenizedText = passageFromTokens;
 
