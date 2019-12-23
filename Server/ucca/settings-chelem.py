63c5f7a2a9d2e5e74d37ab3a42f7676109a6655f
deborah <deborah.nacache@gmail.com>
2019-12-23 09:34:40 +0200

    new database from omri

--- Server/ucca/settings-chelem.py
+++ Server/ucca/settings-chelem.py
@@ -1,7 +1,7 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ucca-dev',
+        'NAME': 'ucca_new',
         'OPTIONS': {
             'options': '-c search_path=django,ucca'
         },
