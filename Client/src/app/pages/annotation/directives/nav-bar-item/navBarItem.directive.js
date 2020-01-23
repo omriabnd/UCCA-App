ec0ad62d432982ed961ac9a11bddf92b6ba5d624
deborah <deborah.nacache@gmail.com>
2020-01-23 15:09:01 +0200

    of part of retokenized token is a unit , token cannot be reunited

--- Client/src/app/pages/annotation/directives/nav-bar-item/navBarItem.directive.js
+++ Client/src/app/pages/annotation/directives/nav-bar-item/navBarItem.directive.js
@@ -1,19 +1,19 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('zAdmin.annotation.directives')
-        .directive('navBarItem',navBarItemDirective);
-        
+        .directive('navBarItem', navBarItemDirective);
+
 
     function navBarItemDirective() {
         var directive = {
-            restrict:'E',
-            templateUrl:'app/pages/annotation/directives/nav-bar-item/navBarItem.html',
-            scope:{
-                imagePath:'=',
-                toolTip:'=',
-                itemObject:'='
+            restrict: 'E',
+            templateUrl: 'app/pages/annotation/directives/nav-bar-item/navBarItem.html',
+            scope: {
+                imagePath: '=',
+                toolTip: '=',
+                itemObject: '='
             },
             link: linkFunction,
             controller: ItemController,
@@ -32,29 +32,99 @@
     }
 
     /** @ngInject */
-    function ItemController($scope, DataService,selectionHandlerService) {
-        //test debo
+    function ItemController($scope, DataService, selectionHandlerService) {
 
-
-        function enableRetokenizeButton(){
+        function partsOfRetokenizedToken() {
             var selectedTokenList = selectionHandlerService.getSelectedTokenList();
-            
-            if(selectedTokenList.length == 0){
+
+            if (selectedTokenList[0] != undefined) {
+
+                if (selectedTokenList[0].static.splitByTokenization == true) {
+                    var arr = [];
+                    var rightTokens = checkRight(selectedTokenList[0], arr);
+                    var rightAndLeftTokens = checkLeft(selectedTokenList[0], rightTokens);
+                    return rightAndLeftTokens;
+                }
+                else {
+                    var arr = []
+                    var onlyRightTokens = checkRight(selectedTokenList[0], arr);
+                    return onlyRightTokens
+                }
+            }
+        };
+
+        function checkRight(selectedTokenList, arr) {
+
+            if (selectedTokenList != undefined) {
+
+                var index = selectedTokenList.indexInUnit;
+
+                if (DataService.tree.tokens[index].static.splitByTokenization == true) {
+                    arr.push(DataService.tree.tokens[index])
+                    checkRight(DataService.tree.tokens[index + 1], arr)
+                }
+                else {
+
+                    if (arr.length == 0) {
+                        arr.push(DataService.tree.tokens[index])
+                        checkRight(DataService.tree.tokens[index + 1], arr)
+                    }
+
+                }
+                return arr
+            }
+        };
+        function checkLeft(selectedTokenList, arr) {
+
+            if (selectedTokenList != undefined) {
+                var index = selectedTokenList.indexInUnit;
+                if (arr.length == 0) {
+                    arr.push(DataService.tree.tokens[index])
+                }
+                if (DataService.tree.tokens[index - 1].static.splitByTokenization == true) {
+                    arr.push(DataService.tree.tokens[index - 1])
+                    checkLeft(DataService.tree.tokens[index - 1], arr)
+                }
+                else {
+                    arr.push(DataService.tree.tokens[index - 1])
+
+                }
+                return arr
+            }
+        }
+
+        function enableRetokenizeButton() {
+            var notAParent = true
+            var listOfTokens = partsOfRetokenizedToken();
+            if (listOfTokens != undefined) {
+                for (var i = 0; i < listOfTokens.length; i++) {
+                    if (listOfTokens[i].inChildUnitTreeId != null) {
+                        notAParent = false;
+                    }
+                    if (notAParent == false) {
+                        return false;
+                    }
+                }
+            }
+            var selectedTokenList = selectionHandlerService.getSelectedTokenList();
+            console.log(DataService.tree)
+
+            if (selectedTokenList.length == 0) {
                 return false;
             }
-            if (selectedTokenList[0].unitTreeId!=0){
+            if (selectedTokenList[0].unitTreeId != 0) {
                 return false
             }
-            else if(selectedTokenList.length >1){
-                    return false;
-                }
-                else{
+            else if (selectedTokenList.length > 1) {
+                return false;
+            }
+            else {
                 var selectionList = selectionHandlerService.getSelectedTokenList();
-                var tokenIntUnit = selectionList[0].inChildUnitTreeId ;
-                if(tokenIntUnit==null){
+                var tokenIntUnit = selectionList[0].inChildUnitTreeId;
+                if (tokenIntUnit == null) {
                     return true;
                 }
-                else {return false}
+                else { return false }
             }
         }
         // Injecting $scope just for comparison
@@ -62,27 +132,27 @@
         var annotationPageVM = $scope.$parent.vm;
         vm.itemClicked = itemClicked;
         vm.checkRetokenizeButton = checkRetokenizeButton
-        vm.enableRetokenizeButton=enableRetokenizeButton
+        vm.enableRetokenizeButton = enableRetokenizeButton
 
-        function checkRetokenizeButton(data){
-            if(data.name == 'Retokenize'){
-               return  enableRetokenizeButton();
+        function checkRetokenizeButton(data) {
+            if (data.name == 'Retokenize') {
+                return enableRetokenizeButton();
             }
             return true
         }
-        
-        if(vm.itemObject.showWhenFull){
+
+        if (vm.itemObject.showWhenFull) {
             vm.checkIfTaskHasComment = checkIfTaskHasComment;
         }
 
-        function checkIfTaskHasComment(){
+        function checkIfTaskHasComment() {
             return DataService.serverData && DataService.serverData.user_comment !== "";
         }
-        function itemClicked(functionName){
-            if(annotationPageVM[functionName]){
+        function itemClicked(functionName) {
+            if (annotationPageVM[functionName]) {
                 annotationPageVM[functionName]()
-            }else{
-                console.log("function not exist",functionName);
+            } else {
+                console.log("function not exist", functionName);
             }
         }
     }
